8d020226a698b5f0e4f1675ed794dcbe719edb27
Shubin <shubin.zhong@outlook.com>
2025-02-25 21:44:07 +1100

    add documentation for ListItem component

--- src/components/ListItem.tsx
+++ src/components/ListItem.tsx
@@ -3,26 +3,34 @@ import fallbackAvatar from "../assets/default-avatar.jpeg";
 import classes from "./ListItem.module.css";
 
 export interface User {
+  /** The fullname of the User */
   contactName: string;
+  /** The email address of the User */
   email: string;
+  /** A profile image URL */
   avatar: string;
+  /** Is the User in active state */
   isActive: boolean;
+  /** Is the User in enabled state */
   isEnabled: boolean;
 }
 export interface ListItemProps extends User {
+  /** To show or hide the email address */
   showEmail: boolean;
 }
 
 export const ListItem = ({
   isActive = false,
   isEnabled = false,
-  showEmail,
-  contactName,
-  email,
-  avatar,
+  showEmail = false,
+  contactName = "",
+  email = "",
+  avatar = "",
   ...props
 }: ListItemProps) => {
+  // If no profile image provided, show default image
   const imageSrc = avatar || fallbackAvatar;
+
   return (
     <div
       id="list-item"
